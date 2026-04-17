5e21c3def6657d514404f2e2e6d8188598d8f5d1
noyalit <noyalit@post.bgu.ac.il>
2026-04-17 15:32:26 +0300

    updating pom and creating project structure

---
+++ Ticketmaster/src/test/java/com/software_project_team_15b/Ticketmaster/TicketmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_project_team_15b.Ticketmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
