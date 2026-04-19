3dd1f0343788e4a24411ab2318212d7ada92719a
oriakas <oriakas@post.bgu.ac.il>
2026-04-19 17:45:32 +0300

    Created the Member class + getters and setters

---
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/Member/Member.java
@@ -0,0 +1,31 @@
+package com.software_project_team_15b.Ticketmaster.Domain.Member;
+
+public class Member {
+    private String username;
+    private String password;
+    private Role role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
