bc0d30a25794071b1f6fac981398dce63fb9353b
shirahad <shirahad@post.bgu.ac.il>
2026-04-20 13:03:59 +0300

    issue-4-done

---
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/Order/OrderHistory.java
@@ -0,0 +1,34 @@
+package com.software_project_team_15b.Ticketmaster.Domain.Order;
+
+import java.util.List;
+
+public class OrderHistory {
+    private String orderId;
+    private String userId;
+    private String eventId;
+    private List<Ticket> tickets;
+
+    public OrderHistory(String orderId, String userId, String eventId, List<Ticket> tickets) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.eventId = eventId;
+        this.tickets = tickets;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+}
