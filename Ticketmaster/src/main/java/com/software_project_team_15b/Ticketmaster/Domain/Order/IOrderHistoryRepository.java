2769aa9cb4d08acdd33a77cea19eef97b133c4b1
shirahad <shirahad@post.bgu.ac.il>
2026-04-20 15:11:39 +0300

    create IOrderHistoryRepository interface with JPA methods for user and event queries

--- Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/Order/IOrderHistoryRepository.java
+++
@@ -1,5 +0,0 @@
-package com.software_project_team_15b.Ticketmaster.Domain.Order;
-
-public interface IOrderHistoryRepository {
-
-}
---
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/OrderHistory/IOrderHistoryRepository.java
@@ -0,0 +1,14 @@
+package com.software_project_team_15b.Ticketmaster.Domain.OrderHistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IOrderHistoryRepository extends JpaRepository<OrderHistory, String> {
+
+    List<OrderHistory> findByUserId(String userId);
+
+    List<OrderHistory> findByEventId(String eventId);
+
+    //TODO: Add query to find order history by companyId
+}
