87921630e5a3abd757324c404012cbef15a3d6e4
shirahad <shirahad@post.bgu.ac.il>
2026-04-23 15:05:49 +0300

    refactor IOrderHistoryRepository: rename method findByEventIds to findByEventIdIn for JPA support and adding imprt for orderhistory

--- Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/OrderHistory/IOrderHistoryRepository.java
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Domain/OrderHistory/IOrderHistoryRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import com.software_project_team_15b.Ticketmaster.Domain.OrderHistory.OrderHistory;
+
 public interface IOrderHistoryRepository extends JpaRepository<OrderHistory, String> {
 
     List<OrderHistory> findByUserId(String userId);
 
     List<OrderHistory> findByEventId(String eventId);
 
-    List<OrderHistory> findByEventIds(List<String> eventIds);
+    List<OrderHistory> findByEventIdIn(List<String> eventIds);
 }
