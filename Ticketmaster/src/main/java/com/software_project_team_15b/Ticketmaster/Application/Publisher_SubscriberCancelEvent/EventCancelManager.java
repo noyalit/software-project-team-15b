972a51d0c27283e1b9123f38e9ac044b7bee0a88
shirahad <shirahad@post.bgu.ac.il>
2026-05-07 15:29:37 +0300

    Implement EventCancelManager and EventSubscriber for event cancellation functionality

---
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Application/Publisher_SubscriberCancelEvent/EventCancelManager.java
@@ -0,0 +1,24 @@
+package com.software_project_team_15b.Ticketmaster.Application.Publisher_SubscriberCancelEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class EventCancelManager {
+    private final List<EventSubscriber> subscribers = new ArrayList<>();
+
+    public void subscribe(EventSubscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    public void unsubscribe(EventSubscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    public void cancelEvent(UUID event) {
+        
+        for (EventSubscriber subscriber : subscribers) {
+            subscriber.notifyEventIsCancelled(event);
+        }
+    }
+}
