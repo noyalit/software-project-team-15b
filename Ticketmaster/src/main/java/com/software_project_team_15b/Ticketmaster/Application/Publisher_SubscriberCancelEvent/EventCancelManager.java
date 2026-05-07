f516ec1711eb8ddd069166c8d5dfcf42db9c363e
shirahad <shirahad@post.bgu.ac.il>
2026-05-07 19:27:33 +0300

    Refactor EventCancelManager to use CopyOnWriteArrayList for thread-safe subscriber management

--- Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Application/Publisher_SubscriberCancelEvent/EventCancelManager.java
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Application/Publisher_SubscriberCancelEvent/EventCancelManager.java
@@ -1,11 +1,14 @@
 package com.software_project_team_15b.Ticketmaster.Application.Publisher_SubscriberCancelEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Component;
+
+@Component // Tells Spring to manage this class as a Singleton
 public class EventCancelManager {
-    private final List<EventSubscriber> subscribers = new ArrayList<>();
+    private final List<EventSubscriber> subscribers = new CopyOnWriteArrayList<>();
 
     public void subscribe(EventSubscriber subscriber) {
         subscribers.add(subscriber);
