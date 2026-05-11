0fb871a635f87bccab0a2d9b4ed204d2bd850a6f
Elad Alfi <eladalfi@walla.com>
2026-05-11 14:56:28 +0300

    small name changes

--- Ticketmaster/src/test/java/com/software_project_team_15b/Ticketmaster/Application/OrderHistoryServiceTests/integration/orderHistoryTestIT.java
+++ Ticketmaster/src/test/java/com/software_project_team_15b/Ticketmaster/Application/OrderHistoryServiceTests/integration/orderHistoryTestIT.java
@@ -46,7 +46,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-class OrderHistoryServiceIT {
+class OrderHistoryTestIT {
 
     @Autowired
     OrderHistoryService service;
