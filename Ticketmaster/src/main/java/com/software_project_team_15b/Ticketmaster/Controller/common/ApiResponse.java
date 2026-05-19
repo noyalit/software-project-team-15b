dd28a7f778dd78af254284e0bb8110947e25ff76
Gabriel Rozental <gabriel.rozental1@gmail.com>
2026-05-19 18:14:23 +0300

    feat: create ApiResponse class for standardized API responses
    Co-authored-by: TomerGelbergBGU <tomergel@post.bgu.ac.il>

---
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Controller/common/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.software_project_team_15b.Ticketmaster.Controller.common;
+
+public class ApiResponse<T> {
+
+    private T data;
+    private String error;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(T data, String error) {
+        this.data = data;
+        this.error = error;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
