ce004f34e74cb7d7ecbc4e789c27b002e79f6489
oriakas <oriakas@post.bgu.ac.il>
2026-06-18 10:31:11 +0300

    fix LotteryServiceWhiteTest tests

--- Ticketmaster/src/test/java/com/software_project_team_15b/Ticketmaster/white/Application/Lottery/LotteryServiceWhiteTest.java
+++ Ticketmaster/src/test/java/com/software_project_team_15b/Ticketmaster/white/Application/Lottery/LotteryServiceWhiteTest.java
@@ -294,6 +294,7 @@ class LotteryServiceWhiteTest {
         when(auth.isTokenValid(TOKEN_A)).thenReturn(true);
         when(auth.extractUserId(TOKEN_A)).thenReturn(USER_A);
         when(userDomainService.isActiveManager(USER_A, COMPANY_ID, EVENT_ID)).thenReturn(false);
+        when(userDomainService.isActiveCompanyManager(USER_A, COMPANY_ID)).thenReturn(false);
         when(userDomainService.isActiveOwner(USER_A, COMPANY_ID)).thenReturn(true);
 
         service.createEventLottery(TOKEN_A, COMPANY_ID, EVENT_ID);
@@ -301,6 +302,7 @@ class LotteryServiceWhiteTest {
         verify(auth).isTokenValid(TOKEN_A);
         verify(auth).extractUserId(TOKEN_A);
         verify(userDomainService).isActiveManager(USER_A, COMPANY_ID, EVENT_ID);
+        verify(userDomainService).isActiveCompanyManager(USER_A, COMPANY_ID);
         verify(userDomainService).isActiveOwner(USER_A, COMPANY_ID);
         verify(lotteryDomainService).createEventLottery(EVENT_ID);
     }
