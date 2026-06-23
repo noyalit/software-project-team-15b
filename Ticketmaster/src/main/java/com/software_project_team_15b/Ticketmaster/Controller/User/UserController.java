c406a835cc2209d1e462b2fb97eb5f79e1573d64
noyalit <noyalit@post.bgu.ac.il>
2026-06-23 19:24:59 +0300

    Add transaction exception import to UserController for improved database error handling

--- Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Controller/User/UserController.java
+++ Ticketmaster/src/main/java/com/software_project_team_15b/Ticketmaster/Controller/User/UserController.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
